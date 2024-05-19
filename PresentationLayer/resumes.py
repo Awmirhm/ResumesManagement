7c16f00910284f88ae450ee526c5210bdc2feee8
Amir <ahossien.mirzaee5@gmail.com>
2024-05-19 11:57:26 +0330

    Update resumes.py files

--- PresentationLayer/resumes.py
+++ PresentationLayer/resumes.py
@@ -1,5 +1,6 @@
 from tkinter import Frame, LabelFrame, Label, Button, Entry, Canvas
 from tkcalendar import DateEntry
+from tkinter.font import Font
 from datetime import date
 from PIL import Image, ImageTk
 
@@ -14,7 +15,8 @@ class Resumes(Frame):
         self.grid_rowconfigure(0, weight=1)
 
         self.canvas = Canvas(self, highlightthickness=0, bd=0)
-        self.canvas.grid_columnconfigure(0,weight=1)
+        self.canvas.grid_columnconfigure(0, weight=1)
+        self.canvas.grid_rowconfigure(0, weight=1)
         self.canvas.grid(row=0, column=0, sticky="nsew")
 
         self.image = None
@@ -23,23 +25,29 @@ class Resumes(Frame):
 
         self.canvas.bind("<Configure>", self.size)
 
-        self.header = LabelFrame(self.canvas, text="Resumes", borderwidth=5)
-        self.header.grid_columnconfigure(1, weight=1)
-        self.header.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="nsew")
+        self.font = Font(family="Comic Sans MS", size=20, weight="bold")
 
-        self.firstname_label = Label(self.header, text="First Name  :")
+        self.font_label = Font(family="Serif", size=14, weight="bold")
+
+        self.header = LabelFrame(self.canvas, text="Form", borderwidth=0, background="#F39F5A", foreground="white",
+                                 labelanchor="n", font=self.font)
+        self.header.grid_columnconfigure(1, weight=1)
+        self.header.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), ipadx=300, ipady=250)
+
+        self.firstname_label = Label(self.header, text="First Name  :", background="#F39F5A", foreground="white",
+                                     font=self.font_label)
         self.firstname_label.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="w")
 
         self.firstname_entry = Entry(self.header)
         self.firstname_entry.grid(row=0, column=1, padx=(0, 10), pady=(10, 10), sticky="ew")
 
-        self.lastname_label = Label(self.header, text="Last Name  :")
+        self.lastname_label = Label(self.header, text="Last Name  :", background="#F39F5A", foreground="white",font=self.font_label)
         self.lastname_label.grid(row=1, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
         self.lastname_entry = Entry(self.header)
         self.lastname_entry.grid(row=1, column=1, padx=(0, 10), pady=(0, 10), sticky="ew")
 
-        self.birthday_label = Label(self.header, text="Birthday  :")
+        self.birthday_label = Label(self.header, text="Birthday  :", background="#F39F5A", foreground="white",font=self.font_label)
         self.birthday_label.grid(row=2, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
         self.birthday_entry = DateEntry(self.header, date_pattern="yyyy-mm-dd")
@@ -49,7 +57,7 @@ class Resumes(Frame):
         self.back.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="sew")
 
     def size(self, event):
-        self.image = Image.open("C:/Users/Maxsys/Pictures/flat-lay-desk-concept-with-copy-space_23-2148459512.jpg")
+        self.image = Image.open("C:/Users/Maxsys/Pictures/black-abstract-dark-3840x2160-9729.jpg")
         self.image_resize = self.image.resize(size=(event.width, event.height))
         self.image_tk = ImageTk.PhotoImage(self.image_resize)
         self.canvas.create_image(0, 0, image=self.image_tk, anchor="nw")

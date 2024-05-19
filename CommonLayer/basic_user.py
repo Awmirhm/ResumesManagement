e1c2313625f39d1698a042e5e094d4e21e05b586
Amir <ahossien.mirzaee5@gmail.com>
2024-05-19 23:09:57 +0330

    Edit file name

--- CommonLayer/basic_user.py
+++ CommonLayer/basic_user.py
@@ -11,17 +11,6 @@ class User(ABC):
     def create_with_tuple(self, data: tuple):
         pass
 
-
-class UsersForm(User):
-    def __init__(self, id, firstname, lastname, birthday, gender, skills, email):
-        super().__init__(id, firstname, lastname)
-
-        self.birthday = birthday
-        self.gender = gender
-        self.skills = skills
-        self.email = email
-
-    @classmethod
-    def create_with_tuple(cls, data: tuple):
-        return cls(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
-
+    @abstractmethod
+    def create_whit_list(self, data: list):
+        pass

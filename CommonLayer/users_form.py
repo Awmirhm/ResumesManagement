c60cb7ee21afe63fb5202533baa8ba7e703201eb
Amir <ahossien.mirzaee5@gmail.com>
2024-05-20 00:31:34 +0330

    Update users_form.py files

--- CommonLayer/users_form.py
+++ CommonLayer/users_form.py
@@ -2,18 +2,19 @@ from .basic_user import User
 
 
 class UsersForm(User):
-    def __init__(self, id, firstname, lastname, birthday, gender, skills, email):
+    def __init__(self, id, firstname, lastname, birthday, gender, skills, email, photo):
         super().__init__(id, firstname, lastname)
 
         self.birthday = birthday
         self.gender = gender
         self.skills = skills
         self.email = email
+        self.photo = photo
 
     @classmethod
     def create_with_tuple(cls, data: tuple):
-        return cls(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        return cls(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])
 
     @classmethod
     def create_whit_list(cls, data: list):
-        return cls(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        return cls(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])

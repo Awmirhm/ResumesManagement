d3b747b303eb4149aa1e22d5dabf652cbb494175
Amir <ahossien.mirzaee5@gmail.com>
2024-05-20 01:40:25 +0330

    Update resumes.py

--- PresentationLayer/resumes.py
+++ PresentationLayer/resumes.py
@@ -33,37 +33,39 @@ class Resumes(Frame):
 
         self.font_label = Font(family="Serif", size=14, weight="bold")
 
-        self.header = LabelFrame(self.canvas, text="Form", borderwidth=0, background="#F39F5A", foreground="white",
+        self.font_entry = Font(family="Serif", size=12, weight="bold")
+
+        self.header = LabelFrame(self.canvas, text="Form", borderwidth=0, background="#F39F5A", foreground="#31363F",
                                  labelanchor="n", font=self.font)
         self.header.grid_columnconfigure(1, weight=1)
         self.header.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), ipadx=500, ipady=200)
 
         # First Name
-        self.firstname_label = Label(self.header, text="First Name :", background="#F39F5A", foreground="white",
+        self.firstname_label = Label(self.header, text="First Name :", background="#F39F5A", foreground="#31363F",
                                      font=self.font_label)
         self.firstname_label.grid(row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="w")
 
-        self.firstname_entry = Entry(self.header, borderwidth=0)
+        self.firstname_entry = Entry(self.header, borderwidth=2, background="#EAD7BB",font=self.font_entry)
         self.firstname_entry.grid(row=0, column=1, padx=(0, 10), pady=(10, 10), sticky="ew")
 
         # Last Name
-        self.lastname_label = Label(self.header, text="Last Name :", background="#F39F5A", foreground="white",
+        self.lastname_label = Label(self.header, text="Last Name :", background="#F39F5A", foreground="#31363F",
                                     font=self.font_label)
         self.lastname_label.grid(row=1, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
-        self.lastname_entry = Entry(self.header, borderwidth=0)
+        self.lastname_entry = Entry(self.header, borderwidth=2, background="#EAD7BB",font=self.font_entry)
         self.lastname_entry.grid(row=1, column=1, padx=(0, 10), pady=(0, 10), sticky="ew")
 
         # Birthday
-        self.birthday_label = Label(self.header, text="Birthday :", background="#F39F5A", foreground="white",
+        self.birthday_label = Label(self.header, text="Birthday :", background="#F39F5A", foreground="#31363F",
                                     font=self.font_label)
         self.birthday_label.grid(row=2, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
-        self.birthday_entry = DateEntry(self.header, date_pattern="yyyy-mm-dd", borderwidth=0)
+        self.birthday_entry = DateEntry(self.header, date_pattern="yyyy-mm-dd", font=self.font_entry)
         self.birthday_entry.grid(row=2, column=1, padx=(0, 10), pady=(0, 10), sticky="ew")
 
         # Gender
-        self.gender_label = Label(self.header, text="Gender :", background="#F39F5A", foreground="white",
+        self.gender_label = Label(self.header, text="Gender :", background="#F39F5A", foreground="#31363F",
                                   font=self.font_label)
         self.gender_label.grid(row=3, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
@@ -79,19 +81,19 @@ class Resumes(Frame):
         self.gender_radio_button_female.grid(row=3, column=1, padx=(100, 10), pady=(0, 10), sticky="w")
 
         # Skills
-        self.skill_label = Label(self.header, text="Skills :", background="#F39F5A", foreground="white",
+        self.skill_label = Label(self.header, text="Skills :", background="#F39F5A", foreground="#31363F",
                                  font=self.font_label)
         self.skill_label.grid(row=4, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
-        self.skill_entry = Entry(self.header, borderwidth=0)
+        self.skill_entry = Entry(self.header, borderwidth=2, background="#EAD7BB",font=self.font_entry)
         self.skill_entry.grid(row=4, column=1, padx=(0, 10), pady=(0, 10), sticky="ew")
 
         # Email
-        self.email_label = Label(self.header, text="Email :", background="#F39F5A", foreground="white",
+        self.email_label = Label(self.header, text="Email :", background="#F39F5A", foreground="#31363F",
                                  font=self.font_label)
         self.email_label.grid(row=5, column=0, padx=(10, 10), pady=(0, 10), sticky="w")
 
-        self.email_entry = Entry(self.header, borderwidth=0)
+        self.email_entry = Entry(self.header, borderwidth=2, background="#EAD7BB",font=self.font_entry)
         self.email_entry.grid(row=5, column=1, padx=(0, 10), pady=(0, 10), sticky="ew")
 
         # Photo
@@ -99,20 +101,21 @@ class Resumes(Frame):
             Image.open("C:/Users/Maxsys/Pictures/1375106.png").resize(size=(100, 100)))
 
         self.photo_label = Label(self.header, background="#F39F5A")
-        self.photo_label.grid(row=6, column=1, padx=(0, 10), pady=(0, 10), sticky="w")
+        self.photo_label.grid(row=6, column=1, padx=(0, 50), pady=(0, 10), sticky="e")
 
         self.photo_button = Button(self.header, image=str(self.image_photo_button_tk), borderwidth=0,
                                    background="#F39F5A", text="Choose your photo", compound="top", font=self.font_label,
-                                   command=self.photo_button_clicked)
-        self.photo_button.grid(row=6, column=1, padx=(10, 10), pady=(0, 10), sticky="e")
+                                   command=self.photo_button_clicked, foreground="#31363F")
+        self.photo_button.grid(row=6, column=1, padx=(0, 10), pady=(0, 10), sticky="w")
 
         # Save Button
         self.image_save_button_tk = ImageTk.PhotoImage(
             Image.open("C:/Users/Maxsys/Pictures/floppy-disk_2344287.png").resize(size=(100, 100)))
 
         self.save_button = Button(self.header, image=str(self.image_save_button_tk), background="#F39F5A",
-                                  borderwidth=0, text="Save", compound="top", font=self.font_label, pady=10)
-        self.save_button.grid(row=7, column=1, padx=(0, 10), pady=(0, 10), sticky="w")
+                                  borderwidth=0, text="Save", compound="top", font=self.font_label, pady=10,
+                                  foreground="#31363F")
+        self.save_button.grid(row=7, column=1, padx=(45, 10), pady=(0, 10), sticky="w")
 
         # Back
         self.image_back_button_tk = ImageTk.PhotoImage(
@@ -120,7 +123,7 @@ class Resumes(Frame):
 
         self.back = Button(self.header, text="Back", command=self.back_button, borderwidth=0,
                            image=str(self.image_back_button_tk), background="#F39F5A", compound="top",
-                           font=self.font_label,pady=10)
+                           font=self.font_label, pady=10, foreground="#31363F")
         self.back.grid(row=7, column=1, padx=(0, 50), pady=(0, 10), sticky="e")
 
     def size(self, event):

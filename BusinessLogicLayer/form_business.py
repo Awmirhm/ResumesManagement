37b1baa6212167e87c7d18bd8b405b620c1e7310
Amir <ahossien.mirzaee5@gmail.com>
2024-05-20 13:00:17 +0330

    Add New file

--- BusinessLogicLayer/form_business.py
+++ BusinessLogicLayer/form_business.py
@@ -1,14 +1,14 @@
 from datetime import date
+import re
+from DataAccessLayer.form_data_access import FormDataAccess
 
 
 class FormBusiness:
     def __init__(self):
-        pass
+        self.form_data_access = FormDataAccess()
 
     def get_form(self, firstname, lastname, birthday, gender: int, skills, email, image):
-
-        email_requirements_at_the_rate = ["@"]
-        email_requirements = ["email", "gmail", "yahoo"]
+        pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
         if len(firstname) < 3:
             return [None, "First Name Length should be least 3"]
@@ -33,3 +33,12 @@ class FormBusiness:
 
         if len(email) < 6:
             return [None, "Email Length should be least 6"]
+        if not re.match(pat, email):
+            return [None, "Invalid Email"]
+
+        if not image:
+            return [None, "Please Choose your photo"]
+
+        else:
+            self.form_data_access.get_form(firstname, lastname, date_value, gender, skills, email, image)
+            return ["Your information has been sent successfully \n You will be notified by email", None]

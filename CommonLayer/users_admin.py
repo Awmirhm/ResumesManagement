e38453013e2b8a8df7d794cf22fed98bae53d5c0
Amir <ahossien.mirzaee5@gmail.com>
2024-05-20 18:37:07 +0330

    Update users_admin.py

--- CommonLayer/users_admin.py
+++ CommonLayer/users_admin.py
@@ -1,7 +1,10 @@
 from .basic_user import User
+from .descriptor import LengthDescriptor
 
 
 class UsersAdmin(User):
+    username = LengthDescriptor(3)
+
     def __init__(self, id, firstname, lastname, username, password):
         super().__init__(id, firstname, lastname)
 
@@ -10,8 +13,8 @@ class UsersAdmin(User):
 
     @classmethod
     def create_with_tuple(cls, data: tuple):
-        return cls(data[0], data[1], data[2], data[3], data[4])
+        return cls(data[0], data[1], data[2], data[3], None)
 
     @classmethod
     def create_whit_list(cls, data: list):
-        return cls(data[0], data[1], data[2], data[3], data[4])
+        return cls(data[0], data[1], data[2], data[3], None)

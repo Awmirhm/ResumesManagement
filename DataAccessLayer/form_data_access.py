8db138ac7716dca61a2d777f52fcfd5f4fc23774
Amir <ahossien.mirzaee5@gmail.com>
2024-05-21 19:01:07 +0330

    Update file

--- DataAccessLayer/form_data_access.py
+++ DataAccessLayer/form_data_access.py
@@ -1,5 +1,6 @@
 import sqlite3
 from CommonLayer.users_form import UsersForm
+from io import BytesIO
 
 
 class FormDataAccess:
@@ -9,15 +10,16 @@ class FormDataAccess:
     def get_form(self, firstname, lastname, birthday, gender, skills, email, image):
         with sqlite3.connect(self.data_base_name) as connection:
             cursor = connection.cursor()
-            cursor.execute("""
-                    INSERT INTO Users_Form (
+            cursor.execute(f"""
+        INSERT INTO Users_Form (
                                    first_name,
                                    last_name,
                                    birthday,
                                    gener_id,
                                    skills,
                                    email,
-                                   image
+                                   image,
+                                   status
                                )
                                VALUES (
                                    ?,
@@ -26,6 +28,35 @@ class FormDataAccess:
                                    ?,
                                    ?,
                                    ?,
-                                   ?
-                               )""", [firstname, lastname, birthday, gender, skills, email, image])
+                                   ?,
+                                   "{0}")""", [firstname, lastname, birthday, gender, skills, email, image])
+            connection.commit()
+
+    def get_resumes(self, form_id):
+        with sqlite3.connect(self.data_base_name) as connection:
+            cursor = connection.cursor()
+            data = cursor.execute("""
+                SELECT id,
+                       first_name,
+                       last_name,
+                       birthday,
+                       gener_id,
+                       skills,
+                       email,
+                       image,
+                       status
+                FROM Users_Form
+                WHERE  id = ?""", [form_id]).fetchone()
+            form = UsersForm.create_with_tuple(data)
+            return form
+
+    def update_status(self, status, form_id):
+        with sqlite3.connect(self.data_base_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("""
+                    UPDATE Users_Form
+                    SET status = ?
+                    WHERE id = ?
+                    """, [status, form_id])
+
             connection.commit()
